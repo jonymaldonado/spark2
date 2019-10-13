e2131b33b6870df068f39f8e6747c17a26a174b3
jonymaldonado <jonathan.maldonado86@gmail.com>
2019-10-13 19:08:45 -0300

    avance en el tp

--- src/main/java/controllers/PrincipalController.java
+++ src/main/java/controllers/PrincipalController.java
@@ -1,17 +1,39 @@
 package controllers;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 public class PrincipalController {
-	
+	private Map<String,Object> model=new HashMap<>(); 
 
 
 	public PrincipalController(){
 		
 	}
 	
+	public ModelAndView mostrarPaginaPrincipal(Request req, Response res) {
+ 		ModelAndView vista= new ModelAndView(model,"index.hbs");
+		
+		return vista;
+	}
+	
+	public ModelAndView insertarUsuario(Request req, Response res) {
+ 		ModelAndView insertar= new ModelAndView(model,"usuario.hbs");
+		
+		return insertar;
+	}
+	
+	public ModelAndView ingresarUsuario(Request req, Response res) {
+ 		ModelAndView ingresar= new ModelAndView(model,"usuario_ingreso.hbs");
+		
+		return ingresar;
+	}
+
 
 
 }
--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -1,5 +1,9 @@
 package server;
 
+import org.eclipse.jetty.server.Response;
+import org.hibernate.jpa.boot.scan.spi.PackageInfoArchiveEntryHandler;
+
+import controllers.PrincipalController;
 import controllers.UsuarioController;
 import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -26,17 +30,23 @@ public class Router {
     private static void configure(){
     	
     	UsuarioController usuarioController=new UsuarioController();
+    	PrincipalController paginaController=new PrincipalController();
     	
     	//Spark.get("/saludo",(req,res)->"Hola");
     	Spark.get("/saludo/:nombre",usuarioController::saludo);
     	//Spark.get("/saludo/:nombre",(req,res)->"Hola "+ req.params("apellido")+" "+req.params("nombre"));
-    	Spark.get("/saludo",(req,res)->"Hola "+ req.queryParams("apellido")+" "+req.queryParams("nombre"));
+    	//Spark.get("/saludo",(req,res)->"Hola "+ req.queryParams("apellido")+" "+req.queryParams("nombre"));
     	//Spark.get("/saludo/*/:nombre",(req,res)->"Hola "+ req.splat()[0]+" "+req.params("nombre"));
     	
     	Spark.get("/usuarios", usuarioController::mostrartodos,Router.engine);
     	Spark.get("/usuarios/:id", usuarioController::mostrar,Router.engine);
     	Spark.post("/usuarios/:id", usuarioController::modificar);
-//    	Spark.get("/index/, );
+    	Spark.get("/ingresarUsuario.", usuarioController::mostrartodos,Router.engine);
+    	Spark.get("/index", paginaController::mostrarPaginaPrincipal, Router.engine);
+    	Spark.get("/insertarUsuario", paginaController::insertarUsuario,Router.engine);
+    	Spark.get("/ingresarUsuario", paginaController::ingresarUsuario,Router.engine);
+    	Spark.get("/", paginaController::mostrarPaginaPrincipal, Router.engine);
+
     	
 
     }
